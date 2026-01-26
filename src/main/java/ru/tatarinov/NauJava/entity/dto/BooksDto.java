a545812529223771cc17b37cee55bc1f37139460
at0m1c <konstantintatarinov177@gmail.com>
2026-01-26 12:10:03 +0500

    Create homeworks5.1

---
+++ src/main/java/ru/tatarinov/NauJava/entity/dto/BooksDto.java
@@ -0,0 +1,13 @@
+package ru.tatarinov.NauJava.entity.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class BooksDto {
+    private Long id;
+    private String title;
+    private String authorName;
+    private String authorCountry;
+}
